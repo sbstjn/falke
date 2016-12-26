1a6872bc489814bb48ff93bb43126f7ed0b2aee2
sbstjn <mail@sbstjn.com>
2016-12-26 18:15:06 +0100

    Add index.js

--- functions/skill/index.js
+++ functions/skill/index.js
@@ -1,5 +1,42 @@
-console.log('starting function')
-exports.handle = function(e, ctx, cb) {
-  console.log('processing event: %j', e)
-  cb(null, { hello: 'world' })
-}
+var assert = require('assert');
+
+exports.handle = (event, context, callback) => {
+  assert(event.session, 'Invalid event payload');
+  assert(event.session.application, 'Invalid event payload');
+  assert(event.session.application.applicationId == process.env.APP_ID, "Invalid application ID");
+
+  try {
+    var intent = require('./intents/' + event.request.intent.name.toLowerCase() + '.js');
+  } catch (e) {
+    throw new Error('Unknown intent. Only `Next` or `Check` are available!');
+  }
+
+  intent(
+    event.request.intent.slots, event.request.locale
+  ).then(
+    res => callback(null, {
+      "version": "1.0",
+      "response": {
+        "outputSpeech": {
+          "type": "PlainText",
+          "text": res.text
+        },
+        "card": {
+          "content": res.text,
+          "title": res.title,
+          "type": "Simple"
+        },
+        "reprompt": {
+          "outputSpeech": {
+            "type": "PlainText",
+            "text": ""
+          }
+        },
+        "shouldEndSession": true
+      },
+      "sessionAttributes": {}
+    })
+  ).catch(
+    err => callback(err)
+  )
+};
